83d34e2203a9985dde0cb8b51627aadab174eeac
Ivan Wang <ivanwang1239@gmail.com>
2024-02-08 18:45:34 -0500

    practice

--- demo/lecture4/async_demo/practice.js
+++ demo/lecture4/async_demo/practice.js
@@ -1,5 +1,7 @@
 // URL variables for the APIs
-
+const ageHTML = document.getElementById("ageResult");
+const genderHTML = document.getElementById("genderResult");
+const nationalityHTML = document.getElementById("nationalityResult");
 
 document.getElementById('nameForm').addEventListener('submit', (event) => {
     event.preventDefault();
@@ -15,4 +17,57 @@ document.getElementById('nameForm').addEventListener('submit', (event) => {
     // They should use Promise.all to handle the multiple fetch requests.
     
     // Error handling should also be implemented.
+    const fetchAge = async () => {
+      const response = await fetch(agifyUrl);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return await response.json();
+    };
+
+    const fetchGender = async () => {
+        const response = await fetch(genderizeUrl);
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        return await response.json();
+    }
+
+    const fetchNational = async () => {
+        const response = await fetch(nationalizeUrl);
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        return await response.json();
+    }
+    let factsList = "";
+    let fetchPromises = [];
+
+    fetchPromises.push(
+        fetchAge().then((temp) => {
+            factsList += `<li>Age: ${temp.age} </li>`;
+        })
+    )
+
+    fetchPromises.push(
+        fetchGender().then((temp) => {
+            factsList += `<li>Gender: ${temp.gender} </li>`;
+        })
+    )
+
+    fetchPromises.push(
+        fetchNational().then((temp) => {
+            factsList += `<li>Nationality: ${temp.country} </li>`;
+        })
+    )
+    
+
+    Promise.all(fetchPromises)
+        ageHTML.innerHTML = `<p>${factsList[0]}</p>`;
+        genderHTML.innerHTML = `<p>${factsList[1]}</p>`;
+        nationalityHTML.innerHTML = `<p>${factsList[2]}</p>`;
+    })
+    .catch((error) => {
+      console.error("There was a problem fetching:", error);
+      catHtml.innerHTML = "Failed to fetch.";
 });
